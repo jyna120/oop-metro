8b193c3abbadf89eb06f3964b0be8517497761f8
subkka <beeniaa@naver.com>
2024-04-02 18:02:31 +0900

    Feat: Charger생성/Card 생성 (예진) #7

---
+++ metro/src/main/java/model/vo/card/PrepaidCard.java
@@ -0,0 +1,33 @@
+package model.vo.card;
+
+import java.util.Scanner;
+
+public class PrepaidCard extends Card{
+    int balance = 0;
+
+    public PrepaidCard(int userId, String name, int age, String card) {
+        super(userId, name, age, card);
+    }
+
+    public void chargePrepaid() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("입금액을 입력하세요.");
+        this.balance = sc.nextInt();
+        System.out.println("충전되었습니다!");
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "PrepaidCard{" +
+                "balance=" + balance +
+                '}';
+    }
+}
