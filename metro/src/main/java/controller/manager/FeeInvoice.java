b3669c092990f8be8dfe68d12da954465c0aad6a
subkka <beeniaa@naver.com>
2024-04-02 18:49:29 +0900

    Update: FeeInvoice 승차시 반환 요금 메소드 수정

--- metro/src/main/java/controller/manager/FeeInvoice.java
+++ metro/src/main/java/controller/manager/FeeInvoice.java
@@ -1,5 +1,7 @@
 package controller.manager;
 
+import controller.Gate;
+
 public class FeeInvoice {
     Gate gate = new Gate();
 
@@ -8,44 +10,32 @@ public class FeeInvoice {
 //    cards[userId].getPrepaidCards()[userId].getBalance(); // 선불카드 잔액
 //    cards[userId].getDeferredCards()[userId].getExpense(); // 후불카드 누적 금액
 //    cards[userId].getClimateCards()[userId].isCharged(); // 기후동행 유효 여부
-    String cardCase = "ClimateCard";
-    int balance;
     int price = 1400;
+    String cardCase = "ClimateCard";
+    int balance = 30000;
 
     //탑승했을 때 출력하는 메소드 작성
     //-> 카드를 먼저 체크하고 나이에 따른 기본 요금 보여줌
-    private int rideFare() {
+    private void rideFare(int price, String cardCase, int balance) { // 나이대 별 요금, 카드 종류, 잔액(or 누적 금액)
         switch(cardCase) {
-            case "ClimateCard":
-                System.out.println("기본 요금 : " + price + "잔액 : " );
+            case "DeferredCard":
+                System.out.println("기본 요금 : " + price + "누적 금액 : " + balance);
                 break;
-            case "DeferredCard": break;
-            case "PrepaidCard": break;
+            case "PrepaidCard":
+                System.out.println("기본 요금 : " + price + "잔액 : " + balance);
+                break;
             default:
-                System.out.println("카드 정보가 유효하지 않습니다.");
-                return -1;
+                System.out.println("승차에 실패했습니다. 다시 시도해주세요.");
+                return;
         }
-        return 0;
     }
 
 
 
     //하차했을 때 출력하는 메소드 작성
     //-> 카드 먼저 체크하고 나이 체크하고 이동 정거장 수랑 환승 체크해서 계산
-    private int surcharge() {
-        return 0;
+    private void surcharge() {
+
     }
 
-    // 카드에 따른 잔액 조회
-    private int balanceInquiry(String cardCase) {
-        switch(cardCase) {
-            case "Prepaid": break;
-            case "Deferred": break;
-            case "Climate": break;
-            default:
-                System.out.println();
-                break;
-        }
-        return balance;
-    }
 }
