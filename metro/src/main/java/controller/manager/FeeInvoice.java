037a36d377845706400235ef36abd0e6ead5883f
DESKTOP-2JVLHQE\Park <beeniaa@naver.com>
2024-04-03 22:01:18 +0900

    Update: 메소드 매개변수 변경, 금액 계산식 수정

--- metro/src/main/java/controller/manager/FeeInvoice.java
+++ metro/src/main/java/controller/manager/FeeInvoice.java
@@ -1,17 +1,27 @@
 package controller.manager;
 
+import model.vo.card.Card;
+
+import java.time.LocalDate;
+
 public class FeeInvoice {
     int balance = 0; // 잔액(or 누적 금액) 임시 저장
 
     //탑승했을 때 출력하는 메소드 작성
     //-> 카드를 먼저 체크하고 나이에 따른 기본 요금 보여줌
-    private void rideFare(int price, String cardCase, int balance) { // 나이대 별 요금, 카드 종류, 잔액(or 누적 금액)
-        switch(cardCase) {
-            case "DeferredCard": // 신용카드로 탑승 시
-                System.out.println("기본 요금 : " + price + "누적 금액 : " + balance);
+    public void rideFare(Card card) { // 나이대 별 요금, 카드 종류, 잔액(or 누적 금액)
+        switch(card.getCard()) {
+            case "deferred": // 신용카드로 탑승 시
+                balance = card.getDeferredCard().getExpense() + card.getPrice();
+                System.out.println("기본 요금 : " + card.getPrice() + ", 누적 금액 : " + balance);
                 break;
-            case "PrepaidCard": // 체크카드로 탑승 시
-                System.out.println("기본 요금 : " + price + "잔액 : " + balance);
+            case "prepaid": // 체크카드로 탑승 시
+                balance = card.getPrepaidCard().getBalance() - card.getPrice();
+                System.out.println("기본 요금 : " + card.getPrice() + ", 잔액 : " + balance);
+                break;
+            case "climate": // 기후동행카드로 탑승 시
+                LocalDate date = card.getClimateCard().getEnd();
+                System.out.println("만료일" + date);
                 break;
             default: // 신용/체크 카드가 아닐 때(기후 동행은 호출하지 않아도 된다.)
                 System.out.println("승차에 실패했습니다. 다시 시도해주세요.");
@@ -22,22 +32,32 @@ public class FeeInvoice {
 
     //하차했을 때 출력하는 메소드 작성
     //-> 카드 먼저 체크하고 나이에 해당하는 price, 이동 정거장 수, 환승 체크해서 계산
-    private int surcharge(String cardCase, int price, int stops, int transfer) { // 카드 종류, 나이대 별 요금, 이동 정거장 수, 환승
+    public void surcharge(Card card, int stops, int transfer) { // 카드 종류, 나이대 별 요금, 이동 정거장 수, 환승
         int overTransfer = transfer / 4;
-        int plusPrice = price * overTransfer + (stops - 10) * 50;
+        int plusPrice = card.getPrice() * overTransfer + (stops > 10 ? (stops - 10) / 4 : 0) * 50;
 
-        switch(cardCase) {
-            case "DeferredCard": // 신용카드로 하차 시
+        switch(card.getCard()) {
+            case "deferred": // 신용카드로 하차 시
                 int addPrice = balance + plusPrice; // 누적 금액
                 System.out.println("추가 요금 : " + plusPrice + ", 누적 금액: " + addPrice);
-                return addPrice;
-            case "PrepaidCard": // 체크카드로 하차 시
+                card.getDeferredCard().setExpense(addPrice);
+                break;
+            case "prepaid": // 체크카드로 하차 시
                 int subPrice = balance - plusPrice; // 잔액
-                System.out.println("추가 요금 : " + plusPrice + ", 잔액: " + subPrice);
-                return subPrice;
+                if(subPrice > 0) {
+                    System.out.println("추가 요금 : " + plusPrice + ", 잔액: " + subPrice);
+                    card.getPrepaidCard().setBalance(subPrice);
+                }else {
+                    System.out.println("잔액이 부족합니다. 충전 후 다시 이용해주세요.");
+                }
+                break;
+            case "climate": // 기후동행카드로 하차 시
+                LocalDate date = card.getClimateCard().getEnd();
+                System.out.println("만료일" + date);
+                break;
             default: // 신용/체크 카드가 아닐 때(기후 동행은 호출하지 않아도 된다.)
                 System.out.println("하차에 실패했습니다. 다시 시도해주세요.");
-                return 0;
+                break;
         }
     }
 
