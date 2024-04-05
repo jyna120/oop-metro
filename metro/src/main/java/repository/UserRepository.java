914de9cd6547c15373827455d7a9350b6d31f61a
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-04-05 15:06:52 +0900

    테스트를 위해 사용자 정보 수정

--- metro/src/main/java/repository/UserRepository.java
+++ metro/src/main/java/repository/UserRepository.java
@@ -14,13 +14,13 @@ import java.util.List;
 public class UserRepository {
     public List<Card> savedUser() {
         List<Card> userList = new ArrayList<>();
-        userList.add(new Card(0, "정예진", 24, "climate", new ClimateCard()));
-        userList.add(new Card(1, "나지영", 24, "prepaid", new PrepaidCard()));
+        userList.add(new Card(0, "정예진", 11, "climate", new ClimateCard()));
+        userList.add(new Card(1, "나지영", 17, "prepaid", new PrepaidCard()));
         userList.add(new Card(2, "박수빈", 25, "deferred", new DeferredCard()));
         userList.add(new Card(3, "김동현", 35, "deferred", new DeferredCard(LocalDate.of(2024,3,15), 50000)));
         userList.add(new Card(4, "옥주현", 44, "climate", new ClimateCard(LocalDate.of(2024,3,1))));
-        userList.add(new Card(5, "청소년", 18, "prepaid", new PrepaidCard(2000)));
-        userList.add(new Card(6, "어린이", 3, "deferred", new DeferredCard(LocalDate.of(2024,3,1), 2500)));
+        userList.add(new Card(5, "어린이", 11, "prepaid", new PrepaidCard(2000)));
+        userList.add(new Card(6, "청소년", 15, "deferred", new DeferredCard(LocalDate.of(2024,3,1), 2500)));
         userList.add(new Card(7, "노약자", 66, "prepaid", new PrepaidCard()));
 
         return userList;
